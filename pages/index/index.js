a3d44acf9fe7bbbef5024c36425f30bd0cea2ae9
943161238@qqcom <943161238@qq.com>
2019-09-22 00:23:35 +0800

    04-轮播图实现完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,23 @@
+Page({
+    data: {
+        // 轮播图数组
+        swiperList: []
+    },
+    // 页面的钩子函数，监听页面加载
+    onLoad() {
+        this.getSwiper()
+    },
+    getSwiper() {
+        wx.request({
+            url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata', //仅为示例，并非真实的接口地址
+            success: (res => {
+                console.log(res.data)
+                this.setData({
+                    swiperList: res.data.message
+                })
 
-Page({})
+
+            })
+        })
+    }
+})
