9ace55a56bc416cd26a02905ed1a8eb2eeca15de
943161238@qqcom <943161238@qq.com>
2019-09-22 00:41:19 +0800

    05-首页导航实现完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,37 @@
 Page({
     data: {
         // 轮播图数组
-        swiperList: []
+        swiperList: [],
+
+        // 导航部分数组
+        navitemList: []
     },
     // 页面的钩子函数，监听页面加载
     onLoad() {
-        this.getSwiper()
+        this.getSwiperData()
+        this.getNavItem()
     },
-    getSwiper() {
+
+    // 获取轮播图数据
+    getSwiperData() {
         wx.request({
             url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata', //仅为示例，并非真实的接口地址
             success: (res => {
-                console.log(res.data)
                 this.setData({
                     swiperList: res.data.message
                 })
+            })
+        })
+    },
 
-
+    getNavItem(){
+        wx.request({
+            url: 'https://api.zbztb.cn/api/public/v1/home/catitems', 
+            success: (res => {
+                console.log(res.data)
+                this.setData({
+                    navitemList:res.data.message
+                })
             })
         })
     }
