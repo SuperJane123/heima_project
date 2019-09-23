454f88800a90f998aad7b03d64ff469dae8733a6
943161238@qqcom <943161238@qq.com>
2019-09-23 21:33:12 +0800

    09-分类页面布局和数据渲染完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,4 +1,4 @@
-// pages/category/index.js
+import {request} from '../../request/index'
 Page({
 
   /**
@@ -6,15 +6,26 @@ Page({
    */
   data: {
     // 分类数据
-    
+    cateList: [],
+    // 分类详情数据
+    cateDetail: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getCateList()
   },
 
-
+  getCateList(){
+    request({url:'/categories'})
+    .then(res=>{      
+      this.setData({
+        cateList:res.data.message,
+        cateDetail: res.data.message[0].children
+      })
+      console.log(res.data.message[0].children)
+    })
+  }
 })
