1d9e01423e454b3ddaf53575e1f5ab18b103a414
943161238@qqcom <943161238@qq.com>
2019-09-25 10:27:56 +0800

    11-商品列表tab栏布局完成

--- components/YGtasb/index.js
+++ components/YGtasb/index.js
@@ -4,20 +4,29 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    tabData:{
+      type:Array,
+      value: []
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    // 当前的索引位置
+    currentIndex: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    // 点击切换栏事件
+    handelTap(e){
+      this.setData({
+        currentIndex: e.target.dataset.index
+      })
+    }
   }
 })
