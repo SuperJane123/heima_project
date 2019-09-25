c6f91e191bfcedf264db96c120d2e2ee7bfe75a2
943161238@qqcom <943161238@qq.com>
2019-09-25 21:10:28 +0800

    13-商品列表上拉加载页面功能完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,3 +1,17 @@
+/*1.上拉加载下一页数据
+  1先找到上拉滚动条触底事件（在页面的生命周期事件中见过，onReachBottom）
+  2先判断有没有下一页数据，（总条数。页码，页条数）注意，如果提供的参数一般有页码数的，那请求回来显示的数据数量就是和页条数一样的
+    2.1 先获取到总条数
+     总页数= 总条数/页条数，像上取整     
+     2.2 判断总页数和当前页数，如果当前页大于总页数，就是没有数据
+     2.3 否则就是还有数据，页码数++
+     2.4 需要重新发起异步请求，注意这里有bug
+     2.5 如果没有下一页数据，需要弹窗提示用户即可
+          */
+
+
+
+
 
 
 import {
@@ -36,8 +50,12 @@ Page({
       cid: '',
       pagenum: 1,
       pagesize: 10
-    }
+    },
+
+   
   },
+   // 总条数
+   totalPages:1 ,
 
   // 获取子组件的数据
   handelgetIndex(e) {
@@ -63,12 +81,17 @@ Page({
         data:this.data.QueryParmas
       })
       .then(res => {
-        console.log(res)
+        const {total,goods} = res.data.message
+         // 接口返回数据
+        //  新数据
+         const newGoodsList = goods
+        //  旧数据
+         const beforeGoodsList = this.data.goodsList
+        this.totalPages = Math.ceil(total / this.data.QueryParmas.pagesize) 
         this.setData({
-          goodsList: res.data.message.goods
+          goodsList: [...beforeGoodsList,...newGoodsList]
         })
       })
-
   },
 
 
@@ -89,6 +112,20 @@ Page({
    */
   onReachBottom: function () {
 
+    const {QueryParmas} = this.data
+       
+    
+    //没有数据，弹窗口提示客户
+    if(QueryParmas.pagenum >= this.totalPages){
+      wx.showToast({
+        title: '数据已加载完毕',
+        mask: true,
+      });
+    }else {
+       // 当触底时，页面++
+       QueryParmas.pagenum++
+       this.getGoodsList()
+    }
   },
 
   /**
