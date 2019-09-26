aebcc07b3663bc723aae8ea55f5402b3e2bb3b36
943161238@qqcom <943161238@qq.com>
2019-09-26 22:31:16 +0800

    18-使用async的方式简化获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,6 @@
-// pages/cart/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
+import {getSetting,openSetting,chooseAddress} from '../../request/index'
 Page({
 
   /**
@@ -16,23 +18,64 @@ Page({
    
   },
 
-  // 点击收货地址时触发的事件
-  getAddress(){
-    wx.chooseAddress({
-      success (res) {
-        console.loh(res)
-        // console.log(res.userName)
-        // console.log(res.postalCode)
-        // console.log(res.provinceName)
-        // console.log(res.cityName)
-        // console.log(res.countyName)
-        // console.log(res.detailInfo)
-        // console.log(res.nationalCode)
-        // console.log(res.telNumber)
-      }
-    })
+
+  // // 获取收货地址事件
+  // async handelGetAddress(){
+  //  const result1 = await getSetting()
+  //  console.log(result1) 
+  //  const auth = result1.authSetting['scope.address']
+  //   if(auth === false){
+  //     openSetting()
+  //   }
+  //  const result2 = chooseAddress()
+  // console.log(result2)
+
+  // }
+  handelGetAddress(){
+    this.getUserAddress()
+    
+  //  wx.getSetting({
+  //    success: (result) => {
+  //      const auth = result.authSetting['scope.address']
+  //      if(auth === false){
+  //        wx.openSetting({
+  //          success: (result2) => {
+  //            console.log(result2)
+  //            wx.chooseAddress({
+  //              success: (result3) => {
+  //                console.log(result3)
+  //              },
+               
+           
+  //            });
+               
+  //          },
+         
+  //        });
+           
+  //      }else {
+  //        wx.chooseAddress({
+  //          success: (result4) => {
+  //            console.log(result4)
+  //          },
+  //        });
+           
+  //      }
+  //    },
+     
+  //  });
+     
+      
+  },
+  // 获取用户的收货地址
+ async getUserAddress(){
+    const res1 = await getSetting()
+    console.log(res1)
+    const auth = res1.authSetting['scope.address']
+    if(auth === false){
+      await openSetting()
+    }
+    const res2 = await chooseAddress()
+    console.log(res2)
   }
-
-
-  
 })
--- request/index.js
+++ request/index.js
@@ -50,3 +50,49 @@ export const request = (parmars) => {
 
 
 
+
+// Promise形式的getsetting
+export const getSetting = () => {
+        return new Promise((resolve, reject) => {
+            wx.getSetting({
+                success: (result) => {
+                    resolve(result)
+                },
+                fail: (err) => {
+                    reject(err)
+                },
+            });
+        })
+    }
+
+// Promise形式的openSetting
+
+export const openSetting = () => {
+    return new Promise((resolve, reject) => {
+        wx.openSetting({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+        });
+    })
+}
+
+
+// Promise形式的chooseAddress
+export const chooseAddress=()=>{
+    return new Promise((resolve,reject)=>{
+      wx.chooseAddress({
+        success: (result) => {
+          resolve(result)
+        },
+        fail: (err) => {
+          reject(err);
+        }
+      });
+        
+    })
+  }
+  
