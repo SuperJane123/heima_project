a443ec0123ba8b0f50da44c0bd6860b3f06febc2
943161238@qqcom <943161238@qq.com>
2019-09-26 22:56:57 +0800

    19-完成获取收货地址的按钮切换显示

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 获取地址的数据
+    address: {}
   },
 
 
@@ -15,67 +16,34 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-   
+  //  1.获取缓存中的收货地址,默认值为空字符串。空对象的布尔值也是true
+    const address = wx.getStorageSync('address');
+    // 给address赋值
+    this.setData({ address})
+     console.log(address)
+    
+      
   },
-
-
-  // // 获取收货地址事件
-  // async handelGetAddress(){
-  //  const result1 = await getSetting()
-  //  console.log(result1) 
-  //  const auth = result1.authSetting['scope.address']
-  //   if(auth === false){
-  //     openSetting()
-  //   }
-  //  const result2 = chooseAddress()
-  // console.log(result2)
-
-  // }
   handelGetAddress(){
     this.getUserAddress()
-    
-  //  wx.getSetting({
-  //    success: (result) => {
-  //      const auth = result.authSetting['scope.address']
-  //      if(auth === false){
-  //        wx.openSetting({
-  //          success: (result2) => {
-  //            console.log(result2)
-  //            wx.chooseAddress({
-  //              success: (result3) => {
-  //                console.log(result3)
-  //              },
-               
-           
-  //            });
-               
-  //          },
-         
-  //        });
-           
-  //      }else {
-  //        wx.chooseAddress({
-  //          success: (result4) => {
-  //            console.log(result4)
-  //          },
-  //        });
-           
-  //      }
-  //    },
-     
-  //  });
-     
-      
   },
   // 获取用户的收货地址
  async getUserAddress(){
+  //  获取用户的授权状态
     const res1 = await getSetting()
     console.log(res1)
+    // 判断授权状态
     const auth = res1.authSetting['scope.address']
     if(auth === false){
       await openSetting()
     }
     const res2 = await chooseAddress()
+    res2.detialAdress = res2.provinceName+res2.cityName+res2.countyName+res2.detailInfo
+    this.setData({
+      address: res2
+    })
+    // 储存到本地存储中
+    wx.setStorageSync('address', res2);
     console.log(res2)
   }
 })
