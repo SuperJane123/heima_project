46c3c21a71c1711ce57a179a593de23d5450baef
943161238@qqcom <943161238@qq.com>
2019-10-01 01:43:57 +0800

    25-搜索页面优化防抖功能

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,4 +1,13 @@
-// pages/search/index.js
+/**
+ * 1.给输入框绑定一个input事件
+ *   1.在事件中获取输入框的值
+ *   2.简单做一个非空的判断
+ *   3.把输入框的值发送到后台
+ *   4.把返回的数据渲染到页面即可
+ * 2.在用户输入 一秒后，再发送请求 ---俗称防抖，防止抖动 （通过定时器来清除上一次的输入）
+ * 
+ * 
+ */
 import regeneratorRuntime from '../../lib/runtime/runtime';
 import {request} from '../../request/index'
 
@@ -6,18 +15,28 @@ Page({
   data: {
       queryList:[]
   },
+  timeId : 2,
 
   // 输入事件
    handelSearch(e){
-    this.getQuery(e)
-  },
-  async getQuery(e){
     const {value} = e.detail
-    const res = await request({url:'/goods/qsearch',data:{query:value}})
-    console.log(res)
+    // 如果不是空字符串才发送请求
+    if( value.trim()){
+      // 清除定时器-- 清楚的是上一个输入值的定时器
+      clearTimeout(this.timeId)
+      this.timeId = setTimeout(()=>{
+        this.getQuery(value)
+      },1000)
+      
+    }
+    
+  },
+  async getQuery(value){
+      const res = await request({url:'/goods/qsearch',data:{query:value}})
     this.setData({
       queryList: res
     })
+    
   }
   
  
